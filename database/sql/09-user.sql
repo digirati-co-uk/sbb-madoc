3d5553f1b5e5e7fbf4e441bccbf0d209dd005f7b
stephenwf <stephen.fraser@digirati.com>
2019-10-30 09:57:01 +0100

    Added elasticsearch + indexers

--- database/sql/09-user.sql
+++ database/sql/09-user.sql
@@ -1,5 +1,5 @@
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (email, name, created, modified, password_hash, role, is_active) VALUES ('admin@example.org', 'admin', '2019-10-29 09:55:54', '2019-10-29 09:55:54', '$2y$10$Tp8k0V0pNfchUXMJArJN/eIXZuFRpX9BpxaKPgP213htMnjMkNtj.', 'global_admin', 1);
+INSERT INTO `user` VALUES (1,'admin@example.org','admin','2018-05-31 12:18:39','2018-05-31 12:18:39','$2y$10$Tp8k0V0pNfchUXMJArJN/eIXZuFRpX9BpxaKPgP213htMnjMkNtj.','global_admin',1);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
